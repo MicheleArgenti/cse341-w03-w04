8535162004ef3b9a4fc1a73d317b2fdae0652c86
raghork <michele.argenti000@gmail.com>
2026-01-28 20:18:03 +0100

    W03 Project: Project 2 Part 1 (CRUD Operations)

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API documentation for the CSE341 Node.js Project W03-W04',
   },
   host: 'localhost:3000',
-  schemes: ['http', 'https'],
+  schemes: ['https', 'http'],
 };
 
 const outputFile = './swagger.json';
