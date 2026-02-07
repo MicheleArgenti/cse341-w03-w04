e332e326433d51adf4bfd8dd88129aa7369d279a
raghork <michele.argenti000@gmail.com>
2026-02-07 20:37:46 +0100

    W04 Project: Project 2 Part 2 (Authentication)

--- routes/authors.js
+++ routes/authors.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const authorController = require('../controllers/authorsController');
+const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', authorController.getAllAuthors);
 router.get('/:id', authorController.getAuthorById);
 
-router.post('/', authorController.createAuthor);
-router.put('/:id', authorController.updateAuthor);
-router.delete('/:id', authorController.deleteAuthor);
+router.post('/', isAuthenticated, authorController.createAuthor);
+router.put('/:id', isAuthenticated, authorController.updateAuthor);
+router.delete('/:id', isAuthenticated, authorController.deleteAuthor);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
+const passport = require('passport');
+
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
-router.get('/', (req, res) => {
-  // #swagger.tags = ['Hello World!']
-  res.send(`
-    Hello World!
-    Try to go to these routes:
-    - /books
-    - /authors
-    - /api-docs
-    `);
-});
 router.use('/books', require('./books'));
 router.use('/authors', require('./authors'));
+
+router.get('/login', passport.authenticate('github', (req, res) => { }));
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
